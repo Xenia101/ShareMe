263376b458aa593c7ccf2dd10cc8b3b5b717bed5
RyuaNerin <admin@ryuar.in>
2021-03-04 10:54:29 +0900

    update database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `files` (
 	`id` CHAR(20) NOT NULL COLLATE 'ascii_bin',
+	`remote_addr` VARCHAR(51) NOT NULL COLLATE 'utf8mb4_unicode_ci',
 	`created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
 	`uploaded` TINYINT(1) NOT NULL DEFAULT '0',
 	`filename` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
 	`expires` DATETIME NULL DEFAULT NULL,
-	`lock` INT(11) NOT NULL DEFAULT '0',
+	`downloading` INT(11) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_unicode_ci'
